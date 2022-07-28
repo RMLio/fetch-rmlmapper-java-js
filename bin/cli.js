fd69f40ba4b05a8340509cbef85e9b984232ce01
Ben De Meester <ben.demeester@ugent.be>
2022-07-28 11:47:50 +0200

    added logging statements, better logging config, commander for cli

--- bin/cli.js
+++ bin/cli.js
@@ -7,41 +7,42 @@
 
 const { download } = require('../lib/download-rmlmapper');
 const logger = require('../lib/logger');
+const { program } = require('commander');
+const pkg = require('../package.json');
+const fs = require('fs');
+const path = require('path');
+
+program
+  .name(pkg.name)
+  .description(pkg.description)
+  .version(pkg.version)
+  .option('-g, --githubVersion [version]', 'optional version you want to download, as published on Github (default: "latest")')
+  .option('-f, --filename [rmlmapper.jar]', 'optional filename of the downloaded jar (via relative to the CWD or via absolute path) (default: name of the published asset of the release)')
+  .showHelpAfterError();
 
 // Get arguments from CLI.
-const args = process.argv.slice(2);
+program.parse();
+const options = program.opts();
 
-// Check number of arguments.
-if (args.length > 2) {
-  logger.error(`Arguments: cli.js [version] [filename].
-version: to specify with version you want to download (optional, default: "latest").
-filename: to specify (via relative to the CWD or via absolute path) the filename of the downloaded jar (optional, default: name of the published asset of the release).`);
-  process.exit(1);
-} else {
-  let version = null;
-  let filename = null;
-  if (args[0]) {
-    version = args[0];
-    if (version.startsWith('v')) {
-      version = version.slice(1);
-    }
-  }
-  if (args[1]) {
-    filename = args[1];
-  }
-
-  if (version) {
-    logger.info(`Downloading the RMLMapper v${version}...`);
-  } else {
-    logger.info('Downloading the latest version of the RMLMapper...');
-  }
-
-  download(filename, version, process.cwd())
-    .then(({ tagName, filePath }) => {
-      fs.writeFileSync(path.resolve(process.cwd(), 'rmlmapper-version.txt'), tagName, 'utf8')
-      logger.info(`Download complete. The RMLMapper is available at ${filePath}`);
-    })
-    .catch(e => {
-      logger.error(e);
-    });
+let version = options.githubVersion || null;
+logger.debug(version)
+if (version?.startsWith('v')) {
+  version = version.slice(1);
 }
+logger.debug(`Using version ${version}`)
+let filename = options.filename || null;
+
+if (version) {
+  logger.info(`Downloading the RMLMapper v${version}...`);
+} else {
+  logger.info('Downloading the latest version of the RMLMapper...');
+}
+
+download(filename, version, process.cwd())
+  .then(({ tagName, filePath }) => {
+    fs.writeFileSync(path.resolve(process.cwd(), 'rmlmapper-version.txt'), tagName, 'utf8')
+    logger.info(`Download complete. The RMLMapper is available at ${filePath}`);
+  })
+  .catch(e => {
+    logger.error(e);
+  });
